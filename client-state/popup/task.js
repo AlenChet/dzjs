d2e7c54d39703e8fea3245630733b11a15142697
AlenaChet <chetvernina2016@gmail.com>
2024-01-04 13:37:23 +0300

    two commit popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,18 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const modal = document.getElementById("subscribe-modal");
-    const closeButton = modal.querySelector(".modal__close_times");
+  const modal = document.getElementById("subscribe-modal");
+  const closeButton = modal.querySelector(".modal__close_times");
 
+  const isModalClosed = getCookie("modalClosed");
 
-    const isModalClosed = localStorage.getItem("modalClosed");
-  
-    if (!isModalClosed) {
+  if (!isModalClosed) {
       modal.classList.add("modal_active");
-    }
-  
-    closeButton.addEventListener("click", function () {
+  }
+
+  closeButton.addEventListener("click", function () {
       modal.classList.remove("modal_active");
-  
-      localStorage.setItem("modalClosed", true);
-    });
+
+      setCookie("modalClosed", true);
   });
+
+  function setCookie(name, value, days = 365) {
+      const date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      const expires = "expires=" + date.toUTCString();
+      document.cookie = name + "=" + value + "; " + expires + "; path=/";
+  }
+
+  function getCookie(name) {
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const cookies = decodedCookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          let cookie = cookies[i];
+          while (cookie.charAt(0) === ' ') {
+              cookie = cookie.substring(1);
+          }
+          if (cookie.indexOf(name + "=") === 0) {
+              return cookie.substring(name.length + 1, cookie.length);
+          }
+      }
+      return null;
+  }
+});
+
   
