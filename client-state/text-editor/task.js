3836036fe223e847810eed4bd17c18353e8baef6
AlenaChet <chetvernina2016@gmail.com>
2024-01-04 13:24:53 +0300

    two commit text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,27 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const editor = document.getElementById("editor");
-    const clearButton = createClearButton();
-    const savedText = localStorage.getItem("editorText");
-    if (savedText) {
-      editor.value = savedText;
-    }
-  
-    editor.addEventListener("input", function () {
-      const text = editor.value;
-      localStorage.setItem("editorText", text);
-    });
-  
-    document.body.appendChild(clearButton);
-    clearButton.addEventListener("click", function () {
-      editor.value = "";
-      localStorage.removeItem("editorText");
-    });
-  
-    function createClearButton() {
-      const button = document.createElement("button");
-      button.textContent = "Очистить содержимое";
-      button.style.marginTop = "10px";
-      return button;
-    }
+  const editor = document.getElementById("editor");
+  const clearButton = createClearButton();
+
+  editor.value = localStorage.getItem("editorText") || "";
+
+  editor.addEventListener("input", function () {
+    localStorage.setItem("editorText", editor.value);
   });
-  
+
+  document.body.appendChild(clearButton);
+
+  clearButton.addEventListener("click", function () {
+    editor.value = "";
+    localStorage.removeItem("editorText");
+  });
+
+  function createClearButton() {
+    const button = document.createElement("button");
+    button.textContent = "Очистить содержимое";
+    button.style.marginTop = "10px";
+    return button;
+  }
+});
